73da5d3828b5302db2f0b3ccf84930c347d014d8
Vladyslav Sitalo <sitalov@amazon.com>
2023-06-24 17:07:43 -0700

    Initial migration towards class structure

--- src/data/collection.ts
+++ src/data/collection.ts
@@ -30,6 +30,7 @@ export const defaultLowPriority = [
 type Priority = 'high' | 'default' | 'low'
 
 const isPartOfHierarchy = (ref: RoamEntity) => ref instanceof Page && ref.text.includes('/')
+
 /**
  * what I want is something like:
  * - create groups based on the most common page reference among all entities, excluding the things like factor, interval, TODO, DONE, etc
@@ -40,10 +41,120 @@ const isPartOfHierarchy = (ref: RoamEntity) => ref instanceof Page && ref.text.i
  * exclude the current page
  * ptr, otter transcript
  */
+export class CommonReferencesGrouper {
+    constructor(
+        // plausibly this should be "really low priority" as in - after all possible other groups have been created
+        // don't leave things ungrouped if they can go into one of excluded categories
+        private dontGroupReferencesTo: RegExp[] = defaultExclusions,
+        private groupPriorities: Record<Partial<'high' | 'low'>, RegExp[]> = {
+            low: [...defaultExclusions, ...defaultLowPriority],
+            high: [],
+        },
+        private addReferencesBasedOnAttributes: string[] = ['isa', 'group with'],
+    ) {
+    }
+
+    public group(entities: RoamEntity[]) {
+        // todo when we exclude all the things - just return one group
+
+        // todo how important is dedup? (would it actually be better to show a few larger groups that have overlap?)
+        // todo merge groups that overlap exactly
+        const referenceGroups = this.buildReferenceGroups(entities)
+
+        return this.deduplicateAndSortGroups(referenceGroups)
+    }
+
+    private notExcluded(entity: RoamEntity) {
+        return !this.dontGroupReferencesTo.some(it => it.test(entity.text))
+    }
+
+    private buildReferenceGroups(entities: RoamEntity[]) {
+        const referenceGroups = new Map<string, Map<string, RoamEntity>>()
+
+        function addReferenceToGroup(referenceUid: string, entity: RoamEntity) {
+            const group = referenceGroups.get(referenceUid)
+            if (group) {
+                group.set(entity.uid, entity)
+            } else {
+                referenceGroups.set(referenceUid, new Map([[entity.uid, entity]]))
+            }
+        }
+
+        const addReferencesFromHierarchy = (ref: RoamEntity, entity: RoamEntity) => {
+            if (!isPartOfHierarchy(ref)) return
+            /**
+             * Hierarchy stuff is underdefined, but general heuristic is name/whatever -> name is top
+             * Nested hierarchies is a thing, not handling those for now
+             */
+            const topOfHierarchyName = ref.text.split('/')[0]
+            const topOfHierarchy = Page.fromName(topOfHierarchyName)
+            topOfHierarchy && this.notExcluded(topOfHierarchy) && addReferenceToGroup(topOfHierarchy.uid, entity)
+        }
+
+        const addReferencesFromAttribute = (baseReference: RoamEntity, entity: RoamEntity, attributte: string) => {
+            const isNotAttributeReference = (it: RoamEntity) => it.text !== attributte
+
+            baseReference.firstAttributeBlock(attributte)
+                ?.linkedEntities.filter(isNotAttributeReference).filter(this.notExcluded.bind(this))
+                ?.forEach(ref => addReferenceToGroup(ref.uid, entity))
+        }
+
+        for (const entity of entities) {
+            const linkedEntities = [...entity.getLinkedEntities(true), entity.page]
+            const references = linkedEntities.filter(this.notExcluded.bind(this))
+
+            for (const ref of references) {
+                addReferenceToGroup(ref.uid, entity)
+                addReferencesFromHierarchy(ref, entity)
+
+                this.addReferencesBasedOnAttributes.forEach(attribute => addReferencesFromAttribute(ref, entity, attribute))
+            }
+        }
+        return referenceGroups
+    }
+
+    /**
+     * take the largest group out,
+     * and remove its members from all other groups, which would re-balance the groups
+     * also a good place to find the wholly subsumed groups (they'd end up empty)
+     *
+     * given how this goes, probably doesn't really make sense to sort the sets or something, plausibly heap would help but also as likely to require too much updating
+     */
+    deduplicateAndSortGroups(
+        referenceGroups: Map<string, Map<string, RoamEntity>>,
+    ) {
+        const groupsByPriorities = groupGroupsByPriorities(referenceGroups, this.groupPriorities) as Record<Priority, [string, Map<string, RoamEntity>][]>
+        console.log({groupsByPriorities})
+
+        const result: Array<readonly [string, RoamEntity[]]> = []
+
+        function consumeFrom(priorityGroup: Map<string, Map<string, RoamEntity>>, minGroupSize: number = 1) {
+            while (referenceGroups.size && priorityGroup.size) {
+                const [referenceUid, largestGroup] = pickLargest(priorityGroup)
+                if (largestGroup.size < minGroupSize) break
+
+                result.push([referenceUid, getValues(largestGroup)] as const)
+
+                priorityGroup.delete(referenceUid)
+                referenceGroups.delete(referenceUid)
+
+                removeGroupEntriesFromOtherGroups(referenceGroups, largestGroup)
+            }
+        }
+
+        consumeFrom(new Map(groupsByPriorities.high))
+        consumeFrom(new Map(groupsByPriorities.default), 2)
+        consumeFrom(new Map(groupsByPriorities.low))
+
+        // consume the rest
+        consumeFrom(referenceGroups)
+
+        return new Map<string, RoamEntity[]>([...result])
+    }
+}
+
 export const groupByMostCommonReferences = (
     entities: RoamEntity[],
-    // plausibly this should be "really low priority" as in - after all possible other groups have been created
-    // don't leave things ungrouped if they can go into one of excluded categories
     dontGroupReferencesTo: RegExp[] = defaultExclusions,
     groupPriorities: Record<Partial<'high' | 'low'>, RegExp[]> = {
         low: [...defaultExclusions, ...defaultLowPriority],
@@ -51,104 +162,10 @@ export const groupByMostCommonReferences = (
     },
     addReferencesBasedOnAttributes: string[] = ['isa', 'group with'],
 ) => {
-    // todo when we exclude all the things - just return one group
-
-    // todo how important is dedup? (would it actually be better to show a few larger groups that have overlap?)
-    // todo merge groups that overlap exactly
-    const referenceGroups = buildReferenceGroups(entities, addReferencesBasedOnAttributes, dontGroupReferencesTo)
-
-    return deduplicateAndSortGroups(referenceGroups, groupPriorities)
-}
-
-function buildReferenceGroups(entities: RoamEntity[], addReferencesBasedOnAttributes: string[], dontGroupReferencesTo: RegExp[]) {
-    const referenceGroups = new Map<string, Map<string, RoamEntity>>()
-
-    function addReferenceToGroup(referenceUid: string, entity: RoamEntity) {
-        const group = referenceGroups.get(referenceUid)
-        if (group) {
-            group.set(entity.uid, entity)
-        } else {
-            referenceGroups.set(referenceUid, new Map([[entity.uid, entity]]))
-        }
-    }
-
-    const notExcluded = (entity: RoamEntity) =>
-        !dontGroupReferencesTo.some(it => it.test(entity.text))
-
-    function addReferencesFromHierarchy(ref: RoamEntity, entity: RoamEntity) {
-        if (!isPartOfHierarchy(ref)) return
-        /**
-         * Hierarchy stuff is underdefined, but general heuristic is name/whatever -> name is top
-         * Nested hierarchies is a thing, not handling those for now
-         */
-        const topOfHierarchyName = ref.text.split('/')[0]
-        const topOfHierarchy = Page.fromName(topOfHierarchyName)
-        topOfHierarchy && notExcluded(topOfHierarchy) && addReferenceToGroup(topOfHierarchy.uid, entity)
-    }
-
-    function addReferencesFromAttribute(baseReference: RoamEntity, entity: RoamEntity, attributte: string) {
-        const isNotAttributeReference = (it: RoamEntity) => it.text !== attributte
-
-        baseReference.firstAttributeBlock(attributte)
-            ?.linkedEntities.filter(isNotAttributeReference).filter(notExcluded)
-            ?.forEach(ref => addReferenceToGroup(ref.uid, entity))
-    }
-
-    for (const entity of entities) {
-        const linkedEntities = [...entity.getLinkedEntities(true), entity.page]
-        const references = linkedEntities.filter(notExcluded)
-
-        for (const ref of references) {
-            addReferenceToGroup(ref.uid, entity)
-            addReferencesFromHierarchy(ref, entity)
-
-            addReferencesBasedOnAttributes.forEach(attribute => addReferencesFromAttribute(ref, entity, attribute))
-        }
-    }
-    return referenceGroups
+    return new CommonReferencesGrouper(dontGroupReferencesTo, groupPriorities, addReferencesBasedOnAttributes).group(entities)
 }
 
 const getValues = (largestGroup: Map<string, RoamEntity>) => Array.from(largestGroup.values())
-
-/**
- * take the largest group out,
- * and remove its members from all other groups, which would re-balance the groups
- * also a good place to find the wholly subsumed groups (they'd end up empty)
- *
- * given how this goes, probably doesn't really make sense to sort the sets or something, plausibly heap would help but also as likely to require too much updating
- */
-function deduplicateAndSortGroups(
-    referenceGroups: Map<string, Map<string, RoamEntity>>,
-    groupPriorities: Record<'high' | 'low', RegExp[]>,
-) {
-    const groupsByPriorities = groupGroupsByPriorities(referenceGroups, groupPriorities) as Record<Priority, [string, Map<string, RoamEntity>][]>
-    console.log({groupsByPriorities})
-
-    const result: Array<readonly [string, RoamEntity[]]> = []
-
-    function consumeFrom(priorityGroup: Map<string, Map<string, RoamEntity>>, minGroupSize: number = 1) {
-        while (referenceGroups.size && priorityGroup.size) {
-            const [referenceUid, largestGroup] = pickLargest(priorityGroup)
-            if (largestGroup.size < minGroupSize) break
-
-            result.push([referenceUid, getValues(largestGroup)] as const)
-
-            priorityGroup.delete(referenceUid)
-            referenceGroups.delete(referenceUid)
-
-            removeGroupEntriesFromOtherGroups(referenceGroups, largestGroup)
-        }
-    }
-
-    consumeFrom(new Map(groupsByPriorities.high))
-    consumeFrom(new Map(groupsByPriorities.default), 2)
-    consumeFrom(new Map(groupsByPriorities.low))
-
-    // consume the rest
-    consumeFrom(referenceGroups)
-
-    return new Map<string, RoamEntity[]>([...result])
-}
 
 function removeGroupEntriesFromOtherGroups(
     referenceGroups: Map<string, Map<string, RoamEntity>>,
